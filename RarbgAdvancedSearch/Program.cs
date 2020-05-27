4b503f1ce5830c9c68d708891d6441f1e886ad2f
ashvin.bhuttoo <ashvin.bhuttoo@gmail.com>
2020-05-27 12:04:48 +0400

    added auto updater codes

--- RarbgAdvancedSearch/Program.cs
+++ RarbgAdvancedSearch/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +19,31 @@ namespace RarbgAdvancedSearch
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            if (args.Length == 1 && args[0] == "INSTALLER")
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                FileSystemAccessRule fsar = new FileSystemAccessRule("Users", FileSystemRights.FullControl, AccessControlType.Allow);
+                DirectorySecurity ds = null;
+
+                ds = di.GetAccessControl();
+                ds.AddAccessRule(fsar);
+                di.SetAccessControl(ds);
+
+                Process.Start(Application.ExecutablePath);
+                return;
+            }
+
+            Thread.Sleep(2000);
+            Process[] runningProcesses = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+            if (runningProcesses.Length == 1 || (args.Length == 1 && args[0] == "OVERRIDE_PROCESS_CHECK")) // if its just me or OVERRIDE is set, let me run!
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Updater.Run("RarbgAdvancedSearch");
+                Application.Run(new Main());
+            }                
         }
     }
 }
